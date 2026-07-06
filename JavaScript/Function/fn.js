14e176d2b5e6e68e95f89d44910f1ea19e83f2f3
Nitu Chaudhary <nituchaudhary467@gmail.com>
2026-07-06 13:25:07 +0530

    Function updated

--- JavaScript/Function/fn.js
+++ JavaScript/Function/fn.js
@@ -5,3 +5,47 @@ function executeFunction(fn, name) {
     return fn(name);
 }
 console.log(executeFunction(sayHello, "Alice"));
+
+function greet(){
+    return function(){
+        console.log('Good morning!');
+    };
+}
+let greetMsg = greet();
+greetMsg();
+
+
+
+var saybye=() => {
+    console.log('Good bye!');
+};
+saybye();
+
+
+var add = (a,b) => a+b +' Hello';
+let val =add(12,34);
+console.log(val);
+
+//spread operator and rest operator
+function collect(...args){
+    //rest
+    console.log(...args);
+}
+let values =[1,2,3];
+
+//spread
+collect(...args);
+
+let [val1,val2, ...rest] = [...values];
+console.log(val1);
+console.log(val2);
+console.log(rest);
+
+
+//rest operator
+function sum(...numbers) {
+  return numbers.reduce((total, num) => total + num, 0);
+}
+
+console.log(sum(1, 2, 3, 4));
+// 10
